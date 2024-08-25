dd7715eb235fed24c875b7013e4ef0ed53f38991
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-08-25 23:50:58 +0300

    updated model Location.js

--- backend/database/models/Locations.js
+++ backend/database/models/Locations.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const LocationsSchema = (module.exports = mongoose.Schema({
-	ownerTgId: { type: Number, required: true },
+	ownerTgId: {type: mongoose.Types.ObjectId,, required: true },
+    locationId: {type: mongoose.Types.ObjectId, required: true},
 	isDerrickBought: { type: Boolean, default: false, required: true },
 	derrickLevl: { type: Number, default: 1, required: true },
 	derrickMiningRate: { type: Number, default: 0.05, required: true },
