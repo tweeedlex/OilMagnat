3d33bb4d6011c0ad9a45735d6e91c023cfbea065
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-08-28 01:48:33 +0300

    use webhook false

--- backend/bot/createTelegramBot.js
+++ backend/bot/createTelegramBot.js
@@ -6,30 +6,38 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const createTelegramBot = (db) => {
 	const WEBHOOK_URL = `${process.env.SERVER_URL}/api/bot${BOT_TOKEN}`;
-	const bot = new TelegramBot(BOT_TOKEN, {
-		webHook: {
-			port: process.env.PORT,
-		},
-	});
-	axios
-		.post(`https://api.telegram.org/bot${BOT_TOKEN}/setWebhook`, {
-			url: WEBHOOK_URL,
-			allowed_updates: ["message", "chat_member", "callback_query", "chat_join_request", "my_chat_member"],
-		})
-		.then((response) => {
-			console.log("Webhook set successfully");
-		})
-		.catch((error) => {
-			if (error.response) {
-				console.log("Error response status:", error.response.status);
-				console.log("Error response data:", error.response.data);
-				// console.log("Error response headers:", error.response.headers);
-			} else if (error.request) {
-				console.log("Error request data:", error.request);
-			}
-			console.log("Error config:", error.config);
+	const useWebhook = process.env.USE_WEBHOOK === "true";
+	console.log(useWebhook);
+	let bot;
+	if (useWebhook) {
+		bot = new TelegramBot(BOT_TOKEN, {
+			webHook: {
+				port: process.env.PORT,
+			},
 		});
-
+		axios
+			.post(`https://api.telegram.org/bot${BOT_TOKEN}/setWebhook`, {
+				url: WEBHOOK_URL,
+				allowed_updates: ["message", "chat_member", "callback_query", "chat_join_request", "my_chat_member"],
+			})
+			.then((response) => {
+				console.log("Webhook set successfully");
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.log("Error response status:", error.response.status);
+					console.log("Error response data:", error.response.data);
+					// console.log("Error response headers:", error.response.headers);
+				} else if (error.request) {
+					console.log("Error request data:", error.request);
+				}
+				console.log("Error config:", error.config);
+			});
+	} else {
+		bot = new TelegramBot(BOT_TOKEN, {
+			polling: true,
+		});
+	}
 	console.log("Telegram bot started");
 
 	bot.onText(/^\/start$/, async (msg) => {
--- frontend/src/http/config.js
+++ frontend/src/http/config.js
@@ -1,30 +1,30 @@
 import axios from "axios";
-import {getUser} from "./user";
+import { getUser } from "./user";
 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL ?? "/api",
+	baseURL: import.meta.env.VITE_API_URL ?? "/api",
 });
 
 api.interceptors.request.use((config) => {
-  config.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
-  return config;
+	config.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+	return config;
 });
 
 const apiRequest = async (method, route, options = {}, body = null) => {
-  try {
-    const config = { ...options };
-    if (body) {
-      config.data = body;
-    }
-    const response = await api.request({
-      method,
-      url: route,
-      ...config,
-    });
-    return response.data;
-  } catch (e) {
-    return {error: e, status: e.response?.status, data: e.response?.data};
-  }
+	try {
+		const config = { ...options };
+		if (body) {
+			config.data = body;
+		}
+		const response = await api.request({
+			method,
+			url: route,
+			...config,
+		});
+		return response.data;
+	} catch (e) {
+		return { error: e, status: e.response?.status, data: e.response?.data };
+	}
 };
 
 export default apiRequest;
