80b65097c62da0a2e4693479e9c116ac0857c2bb
krutiy <7545245@ukr.net>
2024-08-28 16:31:21 +0300

    main page header

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -5,6 +5,8 @@ import mapIcon from "../../img/main/map.png"
 import personalIcon from "../../img/main/personal.png"
 import companyIcon from "../../img/main/company.png"
 import profileIcon from "../../img/main/profile.png"
+import bblIcon from "../../img/main/bbl.png"
+import dollarIcon from "../../img/main/dollar.png"
 
 const Main = () => {
   return (
@@ -12,15 +14,22 @@ const Main = () => {
       <header className={styles.header}>
         <div className={styles.info}>
           <div className={styles.infoSide}>
-            <img src={""} alt={""}/>
-            <p>10,000 BBL</p>
+            <img src={bblIcon} width={32} alt={""}/>
+            <p>10,000 <span>BBL</span></p>
           </div>
+          <svg width="0" height="0">
+            <defs>
+              <clipPath id="roundedPolygon" clipPathUnits="objectBoundingBox">
+                <polygon points="0 0, 1 0.01, 0.8 1, 0.2 1.01"/>
+              </clipPath>
+            </defs>
+          </svg>
           <div className={styles.oilStorage}>
             <p className={styles.oilAmount}>231,321</p>
           </div>
           <div className={[styles.infoSide, styles.right].join(" ")}>
-            <p>10,000 $</p>
-            <img src={""} alt={""}/>
+            <img src={dollarIcon} width={32} alt={""}/>
+            <p>10,000 <span>$</span></p>
           </div>
         </div>
         <nav className={styles.navigation}>
