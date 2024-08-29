8e8da6b98385be4492aff4bc9906b72ba8741ecf
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-08-30 00:36:14 +0300

    comment

--- backend/crons/oilPumping.js
+++ backend/crons/oilPumping.js
@@ -4,26 +4,25 @@ const SECONDS_INTERVAL = 5;
 async function oilPumping(db) {
 	const { User } = db;
 	try {
-		await User.updateMany(
-			{
-				$expr: {
-					$lt: ["$burgerEnergy", { $add: [{ $multiply: ["$burgerLevel", 500] }, 500] }],
-				},
-			},
-			[
-				{
-					$set: {
-						burgerEnergy: {
-							$min: [
-								{ $add: [{ $multiply: ["$burgerLevel", 500] }, 500] }, // значение если баланс > 1000
-								{ $add: ["$burgerEnergy", { $multiply: ["$shovelLevel", 5] }] }, // прибавляем енергию умноженную на 5
-							],
-						},
-					},
-				},
-			]
-		);
-
+		// await User.updateMany(
+		// 	{
+		// 		$expr: {
+		// 			$lt: ["$burgerEnergy", { $add: [{ $multiply: ["$burgerLevel", 500] }, 500] }],
+		// 		},
+		// 	},
+		// 	[
+		// 		{
+		// 			$set: {
+		// 				burgerEnergy: {
+		// 					$min: [
+		// 						{ $add: [{ $multiply: ["$burgerLevel", 500] }, 500] }, // значение если баланс > 1000
+		// 						{ $add: ["$burgerEnergy", { $multiply: ["$shovelLevel", 5] }] }, // прибавляем енергию умноженную на 5
+		// 					],
+		// 				},
+		// 			},
+		// 		},
+		// 	]
+		// );
 		// console.log(`Energy given to automining users.`);
 	} catch (error) {
 		console.error("Error giving energy to automining users:", error);
