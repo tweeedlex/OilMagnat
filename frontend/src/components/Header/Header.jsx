9dd3e12d5f76462a0e2374eb76a0223fda642db7
krutiy <7545245@ukr.net>
2024-08-30 03:24:18 +0300

    leaderboard, market, header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./Header.module.scss";
 import bblIcon from "../../img/main/bbl.png";
 import dollarIcon from "../../img/main/dollar.png";
+import {useSelector} from "react-redux";
 
 const Header = ({isVisible = true}) => {
+  const user = useSelector(state => state.user);
+
   return (
     <div className={styles.info} style={{display: (isVisible ? "flex" : "none")}}>
       <div className={styles.infoSide}>
         <img src={bblIcon} width={32} alt={""}/>
-        <p>10,000 <span>BBL</span></p>
+        <p>{user.oilAmount} <span>BBL</span></p>
       </div>
       <svg width="0" height="0">
         <defs>
@@ -18,11 +21,12 @@ const Header = ({isVisible = true}) => {
         </defs>
       </svg>
       <div className={styles.oilStorage}>
-        <p className={styles.oilAmount}>231,321</p>
+        <div className={styles.fill} style={{height: `${user.notClaimedOil / user.maxOilAmount * 100}%`}}></div>
+        <p className={styles.oilAmount}>{user.notClaimedOil}</p>
       </div>
       <div className={[styles.infoSide, styles.right].join(" ")}>
         <img src={dollarIcon} width={32} alt={""}/>
-        <p>10,000 <span>$</span></p>
+        <p>{user.balance?.toFixed(2)} <span>$</span></p>
       </div>
     </div>
   );
