a252d90f15bf5e72ab92fb2f553d8d3df083fbbd
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-08-30 14:05:07 +0300

    fixed market

--- frontend/src/http/market.js
+++ frontend/src/http/market.js
@@ -1,9 +1,9 @@
 import apiRequest from "./config";
 
 export const getCurrency = async () => {
-  return await apiRequest("GET", "/transferMarket/getCurrency");
-}
+	return await apiRequest("GET", "/transferMarket");
+};
 
 export const changeOil = async (oilAmount) => {
-  return await apiRequest("POST", "/transferMarket/changeOil", {},{ oilAmount });
-}
+	return await apiRequest("POST", "/transferMarket/changeOil", {}, { oilAmount });
+};
