9ab77f2a8046f134ce05bac136eee8a2882da888
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-08-30 20:14:44 +0300

    commit workers

--- backend/app/api/webapp/workers/post.js
+++ backend/app/api/webapp/workers/post.js
@@ -1,17 +1,19 @@
 const { Router } = require("express");
 const ApiError = require("../../../exceptions/api-error");
 const authMiddleware = require("../../../middlewares/authMiddleware");
+const { getWorkersPrice } = require("../../../helpers/traders");
 
 module.exports = Router({ mergeParams: true }).post("/workers/hire", authMiddleware, async (req, res, next) => {
 	try {
 		const { db } = req;
 		const { workerType, buyWith = 1 } = req.body; // buyWith 1 - за USD, 2 - за BBL
 		const tgId = req.user.id;
-		let workersListModel = db.WorkersList;
-		let userWorkersModel = db.Workers;
 		let userModel = db.User;
 		if (!workerType) {
 			return next(ApiError.BadRequest("workerType is required field"));
+		}
+		if (![1, 2, 3].includes(workerType)) {
+			return next(ApiError.BadRequest("Invalid type of workerType field"));
 		}
 		if (![1, 2].includes(buyWith)) {
 			return next(ApiError.BadRequest("Invalid type of buyWith field"));
@@ -23,46 +25,41 @@ module.exports = Router({ mergeParams: true }).post("/workers/hire", authMiddlew
 			throw res.status(404).json("User not found");
 		}
 
-		let workersList = await workersListModel.find({ workerType: workerType });
-
-		let userWorkers = await userWorkersModel.find({ ownerTgId: tgId, workerType: workerType }).sort({ workerLevel: -1 }); // Сортировка по убыванию уровня workerLevel
-
-		let highestUserLevelWorker = userWorkers[0] ? userWorkers[0].workerLevel : 0; // Так как мы ограничили до 1 записи, она будет первой
-
-		if (highestUserLevelWorker >= workersList.length) {
-			return next(new ApiError(404, "Maximum worker upgrade level reached"));
-		}
-
-		let newWorker = workersList.find((element) => {
-			return element.workerLevel == highestUserLevelWorker + 1;
-		});
-
-		if (buyWith == 1) {
-			if (user.balance < newWorker.workerPriceUSD) {
-				return next(new ApiError(404, "Insufficient balance for buying new worker"));
+		if (workerType == 1) {
+			if (user.repairWorkerLevel >= 5) {
+				return next(ApiError.BadRequest("Max worker level reached"));
 			}
-			user.balance -= newWorker.workerPriceUSD;
-		} else if (buyWith == 2) {
-			if (user.oilAmount < newWorker.workerPriceBBL) {
-				return next(new ApiError(404, "Insufficient balance for buying new worker"));
+			let workerPrice = getWorkersPrice(user.traderWorkerLevel + 1);
+			if (user.balance <= workerPrice) {
+				return next(ApiError.BadRequest("Not eaugh balance to hire worker"));
 			}
-			user.oilAmount -= newWorker.workerPriceBBL;
+			user.balance -= workerPrice;
+			user.repairWorkerLevel += 1;
+		} else if (workerType == 2) {
+			if (user.traderWorkerLevel >= 5) {
+				return next(ApiError.BadRequest("Max worker level reached"));
+			}
+			let workerPrice = getWorkersPrice(user.traderWorkerLevel + 1);
+			if (user.balance <= workerPrice) {
+				return next(ApiError.BadRequest("Not eaugh balance to hire worker"));
+			}
+			user.balance -= workerPrice;
+			user.traderWorkerLevel += 1;
+		} else if (workerType == 3) {
+			if (user.consultantWorkerLevel >= 5) {
+				return next(ApiError.BadRequest("Max worker level reached"));
+			}
+			let workerPrice = getWorkersPrice(user.traderWorkerLevel + 1);
+			if (user.balance <= workerPrice) {
+				return next(ApiError.BadRequest("Not eaugh balance to hire worker"));
+			}
+			user.balance -= workerPrice;
+			user.consultantWorkerLevel += 1;
 		}
-
-		await userWorkersModel.create({
-			ownerTgId: tgId,
-			workerLevel: newWorker.workerLevel,
-			workerName: newWorker.workerName,
-			workerDescription: newWorker.workerDescription,
-			workerType: newWorker.workerType,
-			workerBonus: newWorker.workerBonus,
-		});
 
 		await user.save();
 
-		userWorkers = await userWorkersModel.find({ ownerTgId: tgId });
-
-		res.json({ user, userWorkers });
+		res.json({ user });
 	} catch (error) {
 		console.error("Error while buying a worker:", error);
 		next(error);
